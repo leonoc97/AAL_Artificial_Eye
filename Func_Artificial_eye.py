935f917c2fcb42e49e8b98208a765abf9390869e
TobiasBurkhardt99 <148750575+TobiasBurkhardt99@users.noreply.github.com>
2023-11-05 20:47:17 +0100

    Add files via upload

---
+++ Func_Artificial_eye.py
@@ -0,0 +1,56 @@
+import speech_recognition as sr
+import cv2
+import math 
+
+def process_text(text):
+    keywords = ["banana", "apple", "cell phone", "cup", "bottle"]  # Note: "cell phone" instead of "cellphone"
+    detected_keywords = [keyword for keyword in keywords if keyword in text]
+    return detected_keywords
+
+
+def listen_for_keyword():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening for keyword...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, timeout=5)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        return None
+    
+
+def plot_box(results, img, target_classes, classNames):
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            cls = int(box.cls[0])
+            if classNames[cls] in target_classes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                print("Confidence --->",confidence)
+
+                # class name
+                print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+            return img
---
+++ Speech_to_Object_detection_1.3.py
@@ -0,0 +1,53 @@
+from ultralytics import YOLO
+import cv2
+import math 
+import keyboard
+import speech_recognition as sr
+import Func_Artificial_eye
+
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]  
+
+# Define the classes you want to detect
+target_classes = [  ]
+detected_text = []
+while True:
+    if keyboard.is_pressed("space"):
+        detected_text = Func_Artificial_eye.listen_for_keyword()
+
+    if detected_text is not None:
+        print(f"Detected: {detected_text}")
+        target_classes = Func_Artificial_eye.process_text (detected_text)
+
+        if target_classes:
+            print(f"Detected keywords: {', '.join(target_classes)}")
+
+    success, img = cap.read()
+    results = model(img, stream=True);
+
+    img = Func_Artificial_eye.plot_box(results,img,target_classes,classNames)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
